801e8e035236de0fe1aad4f69ed02e0a933c286c
Aditya Shibu <as2397@hw.ac.uk>
2024-08-31 15:38:36 +0400

    Added assets/images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,135 @@
 import logo from "./images/logo.png";
 
+// 3D Print images
+import print1 from "./images/3D Print/3d1.jpg";
+import print2 from "./images/3D Print/3d2.jpg";
+import print3 from "./images/3D Print/3d3.jpg";
+
+// 2021-2022 images
+import ge1 from "./images/2021-2022/Ge1.jpg";
+import ge2 from "./images/2021-2022/Ge2.jpg";
+import ge3 from "./images/2021-2022/Ge3.jpg";
+import ge4 from "./images/2021-2022/Ge4.jpg";
+import ge5 from "./images/2021-2022/Ge5.jpg";
+import ge6 from "./images/2021-2022/Ge6.jpg";
+import ge7 from "./images/2021-2022/Ge7.jpg";
+import ge8 from "./images/2021-2022/Ge8.jpg";
+import ge9 from "./images/2021-2022/Ge9.jpg";
+import ge10 from "./images/2021-2022/Ge10.jpg";
+import ge11 from "./images/2021-2022/Ge11.jpg";
+import ge12 from "./images/2021-2022/Ge12.jpg";
+import ge13 from "./images/2021-2022/Ge13.jpg";
+
+// CAD workshop images
+import cad1 from "./images/CADWorkshop/cad1.jpg";
+import cad2 from "./images/CADWorkshop/cad2.jpg";
+import cad3 from "./images/CADWorkshop/cad3.jpg";
+import cad4 from "./images/CADWorkshop/cad4.jpg";
+import cad5 from "./images/CADWorkshop/cad5.jpg";
+
+// F1 Car images
+import f11 from "./images/F1 cars/f11.jpg";
+import f12 from "./images/F1 cars/f12.jpg";
+import f13 from "./images/F1 cars/f13.jpg";
+
+// Hologram images
+import holo1 from "./images/Holograms/holo1.jpg";
+import holo2 from "./images/Holograms/holo2.jpg";
+import holo3 from "./images/Holograms/holo3.jpg";
+
+// Iftaar party images
+import if1 from "./images/Iftaar party/if1.jpg";
+import if2 from "./images/Iftaar party/if2.jpg";
+import if3 from "./images/Iftaar party/if3.jpg";
+import if4 from "./images/Iftaar party/if4.jpg";
+
+// Marsh tower images
+import mtb1 from "./images/MarshTower/mtb1.jpg";
+import mtb2 from "./images/MarshTower/mtb2.jpg";
+import mtb3 from "./images/MarshTower/mtb3.jpg";
+import mtb4 from "./images/MarshTower/mtb4.jpg";
+import mtb5 from "./images/MarshTower/mtb5.jpg";
+
+// Meet the robots images
+import mtr1 from "./images/Meet the robots/mtr1.jpg";
+import mtr2 from "./images/Meet the robots/mtr2.jpg";
+import mtr3 from "./images/Meet the robots/mtr3.jpg";
+
+// Movie night images
+import mn1 from "./images/Move night/mn1.jpg";
+import mn2 from "./images/Move night/mn2.jpg";
+import mn3 from "./images/Move night/mn3.jpg";
+
+// Prom night images
+import pr1 from "./images/prom night/pr1.jpg";
+import pr2 from "./images/prom night/pr2.jpg";
+
+// Rocket League images
+import rl1 from "./images/RL/rl1.jpg";
+import rl2 from "./images/RL/rl2.jpg";
+import rl3 from "./images/RL/rl3.jpg";
+import rl4 from "./images/RL/rl4.jpg";
+
+// Robowars images
+import robowars1 from "./images/Robowars/robowars1.jpg";
+import robowars2 from "./images/Robowars/robowars2.jpg";
+import robowars3 from "./images/Robowars/robowars3.jpg";
+import robowars4 from "./images/Robowars/robowars4.jpg";
+import robowars5 from "./images/Robowars/robowars5.jpg";
+
 export {
-    logo
+    logo,
+    print1,
+    print2,
+    print3,
+    ge1,
+    ge2,
+    ge3,
+    ge4,
+    ge5,
+    ge6,
+    ge7,
+    ge8,
+    ge9,
+    ge10,
+    ge11,
+    ge12,
+    ge13,
+    cad1,
+    cad2,
+    cad3,
+    cad4,
+    cad5,
+    f11,
+    f12,
+    f13,
+    holo1,
+    holo2,
+    holo3,
+    if1,
+    if2,
+    if3,
+    if4,
+    mtb1,
+    mtb2,
+    mtb3,
+    mtb4,
+    mtb5,
+    mtr1,
+    mtr2,
+    mtr3,
+    mn1,
+    mn2,
+    mn3,
+    pr1,
+    pr2,
+    rl1,
+    rl2,
+    rl3,
+    rl4,
+    robowars1,
+    robowars2,
+    robowars3,
+    robowars4,
+    robowars5,
 };
